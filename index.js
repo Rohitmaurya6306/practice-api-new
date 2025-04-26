6f01e12d69a8c63139dd2dfd6e0d0f06f481a924
6306rrmm <marohit789@gmail.com>
2025-04-26 18:56:36 +0530

    adding

--- index.js
+++ index.js
@@ -140,10 +140,14 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({
-  credentials: true,
-  origin: process.env.FRONTEND_URL
-}))
+// app.use(cors({
+//   credentials: true,
+//   origin: process.env.FRONTEND_URL
+// }))
+
+app.use(cors());
+
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'))
